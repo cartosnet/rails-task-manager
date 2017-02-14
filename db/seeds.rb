5d6f324e91b3aac6a7a77479e56bd54ec1e8c6a0
Matthieu Cartillier <m.carttos@gmail.com>
2017-02-14 17:03:45 +0100

    first rails app with model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+ Task.create(name: "fitness", description: "faire du fitness")
+ Task.create(name: "1fitness", description: "1faire du fitness")
+ Task.create(name: "2fitness", description: "2aire du fitness")
+ Task.create(name: "3fitness", description: "3faire du fitness")
+ Task.create(name: "4itness", description: "4faire du fitness")
